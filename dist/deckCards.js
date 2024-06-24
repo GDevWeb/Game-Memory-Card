d7c39d1b0bc9fd003ebea1de1614764bba6d19d8
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-06-24 11:08:47 +0200

    Refonte de l'app Game-Memory-Card conversion en TS et HotFix du bug de mauvaises pair

---
+++ dist/deckCards.js
@@ -0,0 +1,27 @@
+const deckCards = [
+    {
+        name: "apple",
+        src: "./ressources/apple.svg",
+    },
+    {
+        name: "banana",
+        src: "./ressources/banana.svg",
+    },
+    {
+        name: "brocoli",
+        src: "./ressources/brocoli.svg",
+    },
+    {
+        name: "cherry",
+        src: "./ressources/cherry.svg",
+    },
+    {
+        name: "pepper",
+        src: "./ressources/pepper.svg",
+    },
+    {
+        name: "straw",
+        src: "./ressources/straw.svg",
+    },
+];
+export default deckCards;
